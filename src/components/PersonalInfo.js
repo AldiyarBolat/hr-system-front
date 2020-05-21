6d56b00ce89ec86cb5e65a22df1da742142c3a6b
AldiyarBolat <bolat.aldiar1@gmail.com>
2020-05-21 17:41:33 +0500

    added something

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ProfilePicture from './ProfilePicture';
+import PersonInfo from './PersonInfo';
+
 
 class PersonalInfo extends React.Component{
 	render(){
 		return (
 			<div>
 
+				<div class="modal-body row">
+				  <div className="col-md-4">
+				  	<ProfilePicture/>
+				  </div>
+				  <div className="col-md-8">
+				  	<PersonInfo/>
+				  </div>
+				</div>
+				
+				
 			</div>
 		)
 	}
--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ProfilePicture from './ProfilePicture';
 import MenuBar from './MenuBar';
 import PersonalInfo from './PersonalInfo'
+import PersonToPositions from './PersonToPositions'
+
 
 class ProfilePage extends React.Component{
 	render(){
 		return (
 			<div>
 				<MenuBar/>
+				<PersonalInfo/>
+				<PersonToPositions/>
 			</div>
 		)
 	}
