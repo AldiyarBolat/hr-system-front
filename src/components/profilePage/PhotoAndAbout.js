856c872dc2be45a2dba9187bf1ba2bdfa96fd13f
AldiyarBolat <bolat.aldiar1@gmail.com>
2020-05-24 12:45:37 +0500

    updated css of profile page

--- src/components/profilePage/PhotoAndAbout.js
+++ src/components/profilePage/PhotoAndAbout.js
@@ -7,12 +7,12 @@ import image_src_mock from './image_src_mock';
 class PhotoAndAbout extends React.Component{
 	render(){
 		return (
-			<div class="container-fluid">
+			<div class="container-fluid photoAndAbout align-self-center">
 			  <div class="row">
-			    <div class="col-sm-3 col-md-6 col-lg-4">
+			    <div class="col-sm-3 col-md-6 col-lg-5">
 			    	<ProfilePicture picInfo={image_src_mock[0]}/>
 			    </div>
-			    <div class="col-sm-9 col-md-6 col-lg-8">
+			    <div class="col-sm-9 col-md-6 col-lg-7">
 			    	<PersonInfo item={mock_person_info[0]}/>
 			    </div>
 			  </div>
--- src/components/profilePage/WorkHistory.js
+++ src/components/profilePage/WorkHistory.js
@@ -5,20 +5,22 @@ import { Timeline, Event } from "react-timeline-scribble";
 class WorkHistory extends React.Component{
 	render(){
 		return (
-			<Fragment>
-			    <Timeline>
-			      <Event interval={"2016 – 2018"} title={"Designer"}>
-			        Job deskription hereadd
-			        Job deskription hereadd
-			        Job deskription hereadd
-			      </Event>
-			      <Event interval={"2015 – 2016"} title={"Intern"}>
-			        Job deskription hereadd
-			        Job deskription hereadd
-			        Job deskription hereadd
-			      </Event>
-			    </Timeline>
-			  </Fragment>
+			<div class="d-flex justify-content-center workHistory">
+				<Fragment >
+				    <Timeline>
+				      <Event interval={"2016 – 2018"} title={"Designer"}>
+				        Job deskription hereadd
+				        Job deskription hereadd
+				        Job deskription hereadd
+				      </Event>
+				      <Event interval={"2015 – 2016"} title={"Intern"}>
+				        Job deskription hereadd
+				        Job deskription hereadd
+				        Job deskription hereadd
+				      </Event>
+				    </Timeline>
+				  </Fragment>
+			  </div>
 		)
 	}
 }
